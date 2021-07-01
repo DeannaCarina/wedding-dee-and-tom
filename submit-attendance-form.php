f56b240e484c5973c2f80522021261f6c3e03aaf
DeannaCarina <deannacarina@hotmail.com>
2021-07-01 15:12:22 +0000

    ?fix email submission?

--- submit-attendance-form.php
+++ submit-attendance-form.php
@@ -1,32 +1,28 @@
+
 <?php
-if(isset($_POST['email'])) {
-    $email_to = "deannacarina@gmail.com";
-    $email_subject = "Someone is confirming their attendance!";
-    $first_name = $_POST['first_name']; // required
-    $last = $_POST['last_name']; // required
-    $email_from = $_POST['email_address']; // required
-    $makeit = $_POST['makit']; // required
-    $makeit_message = $_POST['makeit_message']; //not required
-
-    function clean_string($string) {
-    $bad = array("content-type","bcc:","to:","cc:","href");
-    return str_replace($bad,"",$string);
-    }
-
-    $email_message = "Form details below.\n\n";
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_address)."\n";
-    $email_message .= "Can they make it?: ".clean_string($makeit)."\n";
-    $email_message .= "Their message: ".clean_string($makeit_message)."\n";
-
-// create email headers
-$headers = 'From: '.$email_from."\r\n".
-'Reply-To: '.$email_from."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-mail($email_to, $email_subject, $email_message, $headers);
+  $first_name = $_POST['first name'];
+  $last_name = $_POST['last name'];
+  $email_address = $_POST['email'];
+  $makeit = $_POST['can they make it?'];
+  $makeit_message = $_POST['their message:'];
 ?>
 
-  <?php
-}
-?>
+
+<?php
+	$email_from = 'deannas_91@hotmail.com';
+
+	$email_subject = "New Form submission from Wedding Website";
+
+	$email_body = "You have received a new message from the visitor $first_name.\n".
+                            "Here is the message:\n $first_name $last_name ($email_address) says: $makeit_message and $makeit".
+
+  $to = "deannacarina@gmail.com";
+
+  $headers = "From: $first_name \r\n";
+
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
+
+
